de2a88b9f38044f08568a8f689d8acbddea1674b
Andres <AndresEM1524@gmail.com>
2023-10-20 14:41:58 -0700

    Modified snippetView handler to accept an id query

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // handler - executing app logic
@@ -17,7 +19,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Displaying snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
