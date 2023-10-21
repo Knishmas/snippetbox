2b4b70adc7a83b2ace79c6165a00375bec66a1f8
Andres <AndresEM1524@gmail.com>
2023-10-21 12:02:06 -0700

    Modified handlers to serve base template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,14 +14,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	//Note: The base template must be included first!
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "InternalServerError", 500)
