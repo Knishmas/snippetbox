220ba7ff0c812d7abdaf7f5fc73aa0c15355e550
Andres <AndresEM1524@gmail.com>
2023-10-23 17:12:18 -0700

    Added leveled logging through custom loggers (Info and Error)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,6 +12,10 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
 	flag.Parse()
+	//Custom loggers | info & error for leveled logging
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	//init a new servemux
 	mux := http.NewServeMux()
@@ -19,10 +24,10 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on %s", *addr)
+	infoLog.Print("Starting server on %s", *addr)
 	//creating a new server.
 	//Parameters: (TCP network address, your servemux)
 	err := http.ListenAndServe(*addr, mux)
 	//If there's an error, then it's logged.
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
