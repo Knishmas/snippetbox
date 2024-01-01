02e036601e4b846c73a8d8aa89c94bba17d4efb0
Andres <AndresEM1524@gmail.com>
2024-01-01 13:12:11 -0800

    Add current year to templateData struct

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -8,8 +8,9 @@ import (
 )
 
 type templateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -33,11 +34,6 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		}
 
 		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
