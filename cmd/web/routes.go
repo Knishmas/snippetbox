b4662261589fb17e1495ba58182b7d88367d10c6
Andres <AndresEM1524@gmail.com>
2024-02-19 12:31:01 -0800

    Refactor middleware chain to use alice dependency.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,14 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // The routes() method returns a servemux containing our application routes.
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+
+	//create a standard for our middleware chain, used on every req.
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(mux)
 }
